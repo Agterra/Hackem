c337ee645f27b5ad5db8227f6b2e306e1c1dbd30
Louis Le Brun <louis.lebrun3@free.fr>
2016-08-25 16:21:08 +0200

    Creation du projet unity
    Ajout progressif des éléments

---
+++ App/Hackem/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture imgTitre;
+	public Texture imgJouer;
+	public Texture imgDifficulte;
+	public Texture imgOptions;
+	public Texture imgQuitter;
+
+	void onGui(){
+
+		const int buttonWidth = 50;
+		const int buttonHeight = 10; 
+		if (GUI.Button (new Rect (Screen.width / 2 - (buttonWidth / 2), (2 * Screen.height / 5) - (buttonHeight / 2), buttonWidth, buttonHeight), "Jouer")) {
+		}
+			
+	}
+}
